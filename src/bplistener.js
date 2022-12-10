8ddc7506eaf2b0379e4d45a0607cd84e7b5639ce
FAF-D2 <41366065@qq.com>
2022-12-10 15:52:50 +0800

    bpgame 0.2ver

--- src/bplistener.js
+++ src/bplistener.js
@@ -86,7 +86,7 @@ let _start = false;
 function _cmdlistener(e){
     let key = e.key.toLowerCase();
     if(!_start && key == "enter"){
-        bplayer.fullscreen();
+        bplayer.fullscreen(true);
         selector("state", (state)=>{state.style.visibility = "hidden";});
         selector("toast", (toast)=>{toast.style.visibility = "hidden";});
         selector("control", (control)=>{control.style.visibility= "hidden";});
@@ -112,7 +112,7 @@ function _GameIndexInit(index){
     _GameIndex.style.overflow = "hidden";
     _GameIndex.style.border = "none";
     _GameIndex.style.zIndex = 2000000000;
-    _GameIndex.allow = "autoplay";
+    _GameIndex.allow = "autoplay;";
     _GameIndex.style.lineHeight = 0;
     _GameIndex.src = index;
 }
@@ -205,12 +205,19 @@ class Bvideo{
         this.IsPaused = this.video.paused;
         return this.IsPaused;
     }
-    fullscreen(){
+    fullscreen(on){
         if(!this.fullscreen_btn){
             this._init();
         }
-        this.fullscreen_btn.click();
         this.IsFullScreen = (this.video.clientWidth >= window.clientWidth);
+        if(on && !this.IsFullScreen){
+            this.fullscreen_btn.click();
+            return on;
+        }
+        else if(!on && this.IsFullScreen){
+            this.fullscreen_btn.click();
+            return on;
+        }
         return this.IsFullScreen;
     }
     danmaku(on){
@@ -240,13 +247,13 @@ function _total(){
     return bplayer.total();
 }
 function _play(){
-    bplayer.play();
+    return bplayer.play();
 }
 function _pause(){
     return bplayer.pause();
 }
-function _fullscreen(){
-    return bplayer.fullscreen();
+function _fullscreen(on){
+    return bplayer.fullscreen(on);
 }
 function _danmaku(on){
     return bplayer.danmaku(on);
@@ -306,7 +313,13 @@ function _index_receiver(e){
             if(e.data.pipeID){
                 res.pipeID = e.data.pipeID;
             }
-            _GameIndex.contentWindow.postMessage(res, "*");
+            try{
+                _GameIndex.contentWindow.postMessage(res, "*");
+            }
+            catch(err){
+                console.log(err);
+                console.log(e.data, res);
+            }
         }
     }
 }
